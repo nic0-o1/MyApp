feaecbbb7d4af03e5feb8c9efab4fdf2030a580d
Nicolò Rosati <nico.rosati03@gmail.com>
2022-02-18 22:33:43 +0100

    Testing

--- Program.cs
+++ Program.cs
@@ -15,10 +15,9 @@ namespace MyApp
         [STAThread]
         static async Task Main()
         {
-
-            using (var mgr = new UpdateManager(@"C:\Users\nicor\Desktop\MyApp\Releases"))
+            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/nic0-o1/MyApp"))
             {
-                await mgr.UpdateApp();
+                await mgr.Result.UpdateApp();
             }
 
             Application.EnableVisualStyles();
